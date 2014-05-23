e485b9e4339c90ecac530dde30e1dbf2a01270b4
Michael Cramm <gmcramm@gmail.com>
2014-05-22 18:29:17 -0600

    initial

---
+++ test/monarch_blog/test/handler.clj
@@ -0,0 +1,14 @@
+(ns monarch-blog.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        monarch-blog.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
