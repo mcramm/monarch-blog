c913c66c32a91a1f4312322ab8427be2e7d8cf69
Michael Cramm <gmcramm@gmail.com>
2014-05-22 19:14:38 -0600

    Create new posts page, add tests

--- src/monarch_blog/handler.clj
+++ src/monarch_blog/handler.clj
@@ -1,10 +1,29 @@
 (ns monarch-blog.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [hiccup.core :refer [html]]
+            [hiccup.element :refer [link-to]]
+            [hiccup.form :refer [text-field text-area form-to label submit-button]]))
+
+(defn layout [& body]
+  (html body))
+(defn index []
+  (layout
+    [:div.posts
+     (link-to "/posts/new" "New Post")]))
+
+(defn new-post [post]
+  (layout
+    (form-to [:post "/posts"]
+             (label "title" "Title")
+             (text-field "title" (:title post))
+             (label "body" "Body")
+             (text-area "body" (:body post)))))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (index))
+  (GET "/posts/new" {post :params} (new-post post))
   (route/resources "/")
   (route/not-found "Not Found"))
 
--- test/monarch_blog/test/handler.clj
+++ test/monarch_blog/test/handler.clj
@@ -1,14 +1,36 @@
 (ns monarch-blog.test.handler
   (:use clojure.test
-        ring.mock.request  
+        ring.mock.request
         monarch-blog.handler))
 
 (deftest test-app
   (testing "main route"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
+      (is (= (:body response) (layout
+                                [:div.posts
+                                 [:a {:href "/posts/new"} "New Post"]])))))
+  (testing "new post without post param"
+    (let [response (app (request :get "/posts/new"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) (layout
+                                [:form {:action "/posts" :method "POST"}
+                                 [:label {:for "title"} "Title"]
+                                 [:input {:type "text" :id "title" :name "title"}]
+                                 [:label {:for "body"} "Body"]
+                                 [:textarea {:id "body" :name "body"}]]) ))))
+
+  (testing "new post with post param"
+    (let [test-post {:title "Finches" :body "I like finches they are cool."}
+          response (app (request :get "/posts/new" test-post))]
+      (is (= (:status response) 200))
+      (is (= (:body response) (layout
+                                [:form {:action "/posts" :method "POST"}
+                                 [:label {:for "title"} "Title"]
+                                 [:input {:type "text" :id "title" :name "title" :value (:title test-post)}]
+                                 [:label {:for "body"} "Body"]
+                                 [:textarea {:id "body" :name "body"} (:body test-post)]]) ))))
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
